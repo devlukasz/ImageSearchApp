857a46470de52b6d7e98d091b211ba330c57f253
LukaszSmolinskiGIT <44246249+LukaszSmolinskiGIT@users.noreply.github.com>
2020-03-30 21:40:13 +0200

    Making an API request

--- main.js
+++ main.js
@@ -1,13 +1,22 @@
 const form = document.querySelector("form");
 const input = document.querySelector("input");
-
+const client_ID = "ibwbBpBWBhoCVsGknDne18srIpU4v4NjM9RpDOJHLMw";
+const API_URL = `https://api.unsplash.com/search/photos/?client_id=${client_ID}&query=`;
 form.addEventListener("submit", formSubmit);
 
-function searchForm() {
-  form.submit();
-}
 function formSubmit(event) {
   event.preventDefault();
   const searchTerm = input.value;
   console.log(searchTerm);
+
+  search(searchTerm);
+}
+
+function search(searchTerm) {
+  const url = `${API_URL}${searchTerm}`;
+  fetch(url)
+    .then(respone => respone.json())
+    .then(result => {
+      console.log(result);
+    });
 }
