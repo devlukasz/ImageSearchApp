2445895a1b858f6e37d4ce6c8518779739b92de9
LukaszSmolinskiGIT <44246249+LukaszSmolinskiGIT@users.noreply.github.com>
2020-03-30 19:58:35 +0200

    Progress

--- main.js
+++ main.js
@@ -3,6 +3,9 @@ const input = document.querySelector("input");
 
 form.addEventListener("submit", formSubmit);
 
+function searchForm() {
+  form.submit();
+}
 function formSubmit(event) {
   event.preventDefault();
   const searchTerm = input.value;
