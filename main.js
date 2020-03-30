44a89d1d94ab2369681fb5dabb80c73a8833d902
LukaszSmolinskiGIT <44246249+LukaszSmolinskiGIT@users.noreply.github.com>
2020-03-30 22:06:53 +0200

    Display results on page

--- main.js
+++ main.js
@@ -1,7 +1,10 @@
 const form = document.querySelector("form");
 const input = document.querySelector("input");
+const loadingImg = document.querySelector(".loading-image");
+const imageSection = document.querySelector(".images");
 const client_ID = "ibwbBpBWBhoCVsGknDne18srIpU4v4NjM9RpDOJHLMw";
 const API_URL = `https://api.unsplash.com/search/photos/?client_id=${client_ID}&query=`;
+loadingImg.style.display = "none";
 form.addEventListener("submit", formSubmit);
 
 function formSubmit(event) {
@@ -9,14 +12,23 @@ function formSubmit(event) {
   const searchTerm = input.value;
   console.log(searchTerm);
 
-  search(searchTerm);
+  search(searchTerm).then(showImages);
 }
 
 function search(searchTerm) {
   const url = `${API_URL}${searchTerm}`;
-  fetch(url)
+  loadingImg.style.display = "";
+  return fetch(url)
     .then(respone => respone.json())
     .then(result => {
-      console.log(result);
+      return result.results;
     });
+}
+function showImages(images) {
+  images.forEach(image => {
+    const imgElement = document.createElement("img");
+    imgElement.src = image.urls.full;
+    imageSection.appendChild(imgElement);
+  });
+  loadingImg.style.display = "none";
 }
